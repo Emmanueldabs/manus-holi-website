5f0a2b8e4bc06396ee5e3562aafedbd0213b0d7c
Emmanueldabs <100179572+Emmanueldabs@users.noreply.github.com>
2022-03-19 23:54:34 -0700

    Add files via upload

---
+++ hoil .js
@@ -0,0 +1,43 @@
+function f1(){
+    window.alert("Some of the more benign, modern Holi colors, called gulal in Hindi when made in powder form, are a mixture of more than 95% cornstarch blended with food-, drug-, and cosmetic-grade dyes. These pigments, known as FD&C colors in the U.S., are the same ones that bring a rainbow of colors to candy");
+}
+function f2(){
+window.alert("Holi marks the last full-moon day of the lunar month. It's a time of great cheer and fun as people smear bright colours on each other. It also symbolises new beginnings. The day is marked with sumptuous feasts and dancing.");
+}
+function f3(){
+    window.alert("History and rituals. The Holi festival is an ancient Hindu festival with its cultural rituals. It is mentioned in the Puranas, Dasakumara Charita, and by the poet Kālidāsa during the 4th century reign of Chandragupta II. The celebration of Holi is also mentioned in the 7th-century Sanskrit drama Ratnavali.");
+}
+function f4(){
+    window.alert("What is Holi? Rooted in Hindu mythology, Holi also celebrates the triumph of good over evil — symbolism that correlates with the passing of winter. The Holi Festival's history stems from the legend of a female demon and her brother, who believed was the ruler of the universe, according to CNN.");
+}
+function f5(){
+ window.alert("Red symbolizes love and fertility; yellow is the color of turmeric, a powder native to India and used as a natural remedy; blue represents the Hindu God Krishna; and green is for new beginnings.");
+}
+function Red(){
+    document.getElementById("col").src="red splash.png";
+}
+function Orange(){
+    document.getElementById("col").src="orange splash.png";
+}
+function Yellow(){
+    document.getElementById("col").src=" yellow splash.png";
+}
+function Green(){
+    document.getElementById("col").src="green splash.png";
+}
+function Blue(){
+    document.getElementById("col").src="blue splash.png";
+}
+function Purple(){
+    document.getElementById("col").src="purple splash.png";
+}
+function Cyan(){
+    document.getElementById("col").src="cyan splash.png";
+}
+function Violet(){
+    document.getElementById("col").src="violet splash.png";
+}
+
+
+
+
